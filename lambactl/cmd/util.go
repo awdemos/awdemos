c56ce34cc9bcacf59489b38ea6e52b7c341d792c
a <a@as-MacBook-Pro.local>
2024-03-25 11:56:01 -0400

    add missing package

--- lambactl/cmd/util.go
+++ lambactl/cmd/util.go
@@ -1,16 +1,17 @@
-     package cmd
+package cmd
 
-     import (
-         "encoding/json"
-         "fmt"
-     )
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
 
-     // PrettyPrintJSON takes a byte slice and prints formatted JSON
-     func PrettyPrintJSON(input []byte) {
-         var prettyJSON bytes.Buffer
-         error := json.Indent(&prettyJSON, input, "", "    ")
-         if error != nil {
-             fmt.Println("Failed to format JSON", error)
-         }
-         fmt.Println(string(prettyJSON.Bytes()))
-     }
+// PrettyPrintJSON takes a byte slice and prints formatted JSON
+func PrettyPrintJSON(input []byte) {
+	var prettyJSON bytes.Buffer
+	error := json.Indent(&prettyJSON, input, "", "    ")
+	if error != nil {
+		fmt.Println("Failed to format JSON", error)
+	}
+	fmt.Println(string(prettyJSON.Bytes()))
+}
