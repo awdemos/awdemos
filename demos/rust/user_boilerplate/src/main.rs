c67e2292fd4542e098247854dc49c02b8cb9516a
Andrew <159745380+awdemos@users.noreply.github.com>
2025-01-31 17:59:53 -0500

    init

---
+++ demos/rust/user_boilerplate/src/main.rs
@@ -0,0 +1,34 @@
+struct User {
+    username: String,
+    email: String,
+    uri: String,
+    active: bool,
+}
+
+impl User {
+    fn new(username: String, email: String, uri: String) -> Self {
+        User {
+            username,
+            email,
+            uri,
+            active: true,
+        }
+    }
+    fn deactivate(&mut self) {
+        self.active = false;
+    }
+}
+
+fn main() {
+    let mut new_user = User::new(
+        String::from("alanturing"),
+        String::from("alan@turing.com"),
+        String::from("https://turing.com"),
+    );
+    
+    println!("Hello, {}!", new_user.username);
+    println!("Account {} status is: {}", new_user.username, new_user.active);
+    new_user.deactivate();
+    println!("Account {} status is: {}", new_user.username, new_user.active);
+}
+
