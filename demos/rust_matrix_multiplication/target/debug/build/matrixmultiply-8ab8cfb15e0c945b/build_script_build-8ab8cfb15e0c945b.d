3a21093010a64e5c8d793720d65aa0b2f521fb52
Andrew <159745380+awdemos@users.noreply.github.com>
2025-02-28 23:59:07 -0500

    simple matrix multiplicaiton calculator

--- demos/rust_matrix_multiplication/target/debug/build/matrixmultiply-8ab8cfb15e0c945b/build_script_build-8ab8cfb15e0c945b.d
+++
@@ -1,5 +0,0 @@
-/var/home/a/code/demos/demos/rust_matrix_multiplication/target/debug/build/matrixmultiply-8ab8cfb15e0c945b/build_script_build-8ab8cfb15e0c945b: /var/home/a/.cargo/registry/src/index.crates.io-6f17d22bba15001f/matrixmultiply-0.3.9/build.rs
-
-/var/home/a/code/demos/demos/rust_matrix_multiplication/target/debug/build/matrixmultiply-8ab8cfb15e0c945b/build_script_build-8ab8cfb15e0c945b.d: /var/home/a/.cargo/registry/src/index.crates.io-6f17d22bba15001f/matrixmultiply-0.3.9/build.rs
-
-/var/home/a/.cargo/registry/src/index.crates.io-6f17d22bba15001f/matrixmultiply-0.3.9/build.rs:
