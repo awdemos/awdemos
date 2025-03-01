e653c9a2826ddff58d51b5155a8c0c58470d7f83
Andrew <159745380+awdemos@users.noreply.github.com>
2025-02-28 23:59:07 -0500

    simple matrix multiplicaiton calculator

--- demos/rust_matrix_multiplication/target/debug/deps/rust_matrix_multiplication-5e204735d7d343cd.d
+++
@@ -1,5 +0,0 @@
-/var/home/a/code/demos/demos/rust_matrix_multiplication/target/debug/deps/rust_matrix_multiplication-5e204735d7d343cd: src/main.rs
-
-/var/home/a/code/demos/demos/rust_matrix_multiplication/target/debug/deps/rust_matrix_multiplication-5e204735d7d343cd.d: src/main.rs
-
-src/main.rs:
--- demos/rust_matrix_multiplication/target/debug/rust_matrix_multiplication.d
+++
@@ -1,1 +0,0 @@
-/var/home/a/code/demos/demos/rust_matrix_multiplication/target/debug/rust_matrix_multiplication: /var/home/a/code/demos/demos/rust_matrix_multiplication/src/main.rs
